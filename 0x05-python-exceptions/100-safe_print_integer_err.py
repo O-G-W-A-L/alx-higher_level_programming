5b90f312a40628e190ec11228351c61988fdb653
O-G-W-A-L <ogwalja04@gmail.com>
2023-12-18 01:54:58 -0800

    safe with error msg

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+    except (TypeError, ValueError):
+        print('Exception: {}'.format(error), file=sys.stderr)
+        return False
+
+    return True
