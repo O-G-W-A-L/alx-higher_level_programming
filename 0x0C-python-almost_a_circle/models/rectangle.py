ee5fa2d6c5e7544db5e1b1c969baa03942b13d10
O-G-W-A-L <ogwalja04@gmail.com>
2024-01-11 03:19:29 -0800

    display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,8 +68,10 @@ class Rectangle(Base):
 
     def display(self):
         """prints to stdout the rectangle instance with '#' """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """returns the print() and str() rep of a rectangle"""
