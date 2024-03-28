4cd215d49008a369b5105718bff2aebc881d9a21
O-G-W-A-L <ogwalja04@gmail.com>
2024-03-28 05:06:18 -0700

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,7 +10,7 @@ import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-    print("Your email is: {}".format(email))
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
